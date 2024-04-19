231f48e0cd7b5ba4451be1e3d746cd2dae662982
mayur shinde <kssmsslss330@gmail.com>
2024-04-19 14:14:47 +0530

    "ui fixed"

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -15,7 +15,7 @@ const Navbar = async() => {
     <Container>
       <div className='w-full py-5 px-5 border-b flex md:gap-x-10 gap-x-4 items-end'>
         
-      <Link className=' text-black font-bold cursor-pointer block' href="/">M</Link>
+      <Link className=' text-black font-bold cursor-pointer block md:hidden' href="/">M</Link>
         
       <Link className=' text-black font-bold cursor-pointer text-xl  hidden md:block' href="/">MSTORE</Link>
       <MainNav data={data}/>
