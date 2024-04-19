d85118676c82ded8cd8ad270cccebce0fa33560a
mayur shinde <kssmsslss330@gmail.com>
2024-04-19 12:49:50 +0530

    "typos fixed"

--- types.ts
+++ types.ts
@@ -1,44 +1,39 @@
-export interface Category{
-    id:string,
-    name:string, 
-    billboard:Billboard
-}
-
-
-
 export interface Billboard {
-    id:string,
-    label:string,
-    imageUrl:string, 
+    id: string;
+    label: string;
+    imageUrl: string;
 }
 
-export interface Product{
-    id:string,
-    name:string,
-    desc:string,
-    price:string,
-    size:Size[]
-    color:Color[]
-    images:Image[]
-    isFeatured:boolean
-    category:Category
-    
+export interface Category {
+    id: string;
+    name: string;
+    billboard: Billboard;
 }
 
-
-export interface Image{
- id:string
- url:string
+export interface Product {
+    id: string;
+    category: Category;
+    desc:string;
+    name: string;
+    price: string;
+    isFeatured: boolean;
+    size: Size;
+    color: Color;
+    images: Image[]
 }
 
-export interface Size{
-    id:string,
-    name:string,
-    value:string
+export interface Image {
+    id: string;
+    url: string;
 }
 
-export interface Color{
-    id:string,
-    name:string,
-    value:string
+export interface Size {
+    id: string;
+    name: string;
+    value: string;
+}
+export interface Color {
+    id: string;
+    name: string;
+    value: string;
 }
